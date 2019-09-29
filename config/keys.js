f233f28d70434d2b49c943b6e4e1769906ce693e
Shubham Jain <shubham.jain@engineerbabu.in>
2019-09-29 19:11:14 +0530

    dialog flow configuration

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleProjectId: 'react-node-agent-yxoklv',
+    dialogFlowSessionId: 'node-bot-session',
+    dialogFlowSessionLanguageCode: 'en-US'
+}
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,3 +1,9 @@
+const dialogflow = require('dialogflow');
+const config = require('../config/keys');
+const sessionClient = new dialogflow.SessionsClient();
+
+const sessionPath = sessionClient.sessionPath(config.googleProjectId, config.dialogFlowSessionId);
+
 module.exports = app => {
 
     app.get('/', (req, res) => {
@@ -5,6 +11,34 @@ module.exports = app => {
     });
 
     app.post('/api/df_text_query', (req, res) => {
+
+        // The text query request.
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    // The query to send to the dialogflow agent
+                    text: req.body.text,
+                    // The language used by the client (en-US)
+                    languageCode: config.dialogFlowSessionLanguageCode,
+                },
+            },
+        };
+        sessionClient.detectIntent(request)
+            .then(response => {
+                console.log('Detected intent');
+                const result = responses[0].queryResult;
+                console.log(`  Query: ${result.queryText}`);
+                console.log(`  Response: ${result.fulfillmentText}`);
+                if (result.intent) {
+                    console.log(`  Intent: ${result.intent.displayName}`);
+                } else {
+                    console.log(`  No intent matched.`);
+                }
+            })
+            .catch(err => {
+                console.log('ERROR: ', err);
+            })
         res.send({ 'do': 'text query' });
     });
 
